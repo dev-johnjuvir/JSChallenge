e8d822acd2d8e57e495ef43416c6cab47d053689
James Dollente <psalmybald@yahoo.com>
2020-09-22 13:26:30 +0800

    fix:fix search

--- js/app.js
+++ js/app.js
@@ -1,117 +1,109 @@
-$(function(){
-    console.log("ready;");
+$(function () {
+  console.log("ready;");
 
-    //loadModule
-    loadModule();
+  //loadModule
+  loadModules();
 
-    let searchCard = $('.content.card');
-    searchCard.hide();
-    $('#searchBar').keyup(function(){
-   
-        // Search text
-        var text = $(this).val().toLowerCase();
-      
-        // Hide all content class element
-        $('.content').hide();
-     
-        // Search 
-        $('.content .card-title').each(function(){
-      
-         if($(this).text().toLowerCase().indexOf(""+text+"") != -1 ){
-          $(this).closest('.content').show();
-         }
-       });
-    });
-
-  
-    
+  let searchCard = $(".content.card");
+  searchCard.hide();
 });
 
-function loadModule(){
-    $.getJSON( "modules.json", function( data ) {
-        console.log(data);
-        var sections = [];
-        // $.each(data.moduleID, function(data){
-        //   console.log(data.sectionTitle);
-        // });
-        // console.log(data);
-        console.log("the heck");
-    });
+$("#searchBar").keyup(function () {
+  // Search text
+  var text = $(this).val().toLowerCase();
+
+  // Hide all content class element
+  $(".content").hide();
+
+  // Search
+  $(".content .card-title").each(function () {
+    if (
+      $(this)
+        .text()
+        .toLowerCase()
+        .indexOf("" + text + "") != -1
+    ) {
+      $(this).closest(".content").show();
+    }
+  });
+});
+
+function loadModule() {
+  $.getJSON("modules.json", function (data) {
+    console.log(data);
+  });
 }
 
 function supportsLocalStorage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch(e) {
-      return false;
-    }
-
+  try {
+    return "localStorage" in window && window["localStorage"] !== null;
+  } catch (e) {
+    return false;
+  }
 }
 
 function getRecentSearches() {
-    var searches = localStorage.getItem('recentSearches');
-    if(searches) {
-        return JSON.parse(searches);
-    } else {
-        return [];
-    }
+  var searches = localStorage.getItem("recentSearches");
+  if (searches) {
+    return JSON.parse(searches);
+  } else {
+    return [];
+  }
 }
 
 function saveSearchString(str) {
-var searches = getRecentSearches();
-if(!str || searches.indexOf(str) > -1) {
+  var searches = getRecentSearches();
+  if (!str || searches.indexOf(str) > -1) {
     return false;
-}
+  }
 
-    searches.push(str);
-    localStorage.setItem('recentSearches', JSON.stringify(searches));
-    return true;
-
+  searches.push(str);
+  localStorage.setItem("recentSearches", JSON.stringify(searches));
+  return true;
 }
 
 function removeSearches() {
-    localStorage.removeItem('recentSearches');
+  localStorage.removeItem("recentSearches");
 }
 
 // Create an li, given string contents, append to the supplied ul
 function appendListItem(listElement, string) {
-  var listItemElement = document.createElement('A');
-  listItemElement.setAttribute('href', '#');
-  listItemElement.setAttribute('class', 'badge badge-dark');
+  var listItemElement = document.createElement("A");
+  listItemElement.setAttribute("href", "#");
+  listItemElement.setAttribute("class", "badge badge-dark");
   listItemElement.innerHTML = string;
   listElement.appendChild(listItemElement);
 }
 
 // Empty the contents of an element (ul)
 function clearList(listElement) {
-  listElement.innerHTML = '';
+  listElement.innerHTML = "";
 }
 
-window.onload = function() {
+window.onload = function () {
+  if (localStorage) {
+    var searchForm = document.getElementById("searchForm");
+    var searchBar = document.getElementById("searchBar");
+    var recentSearchList = document.getElementById("recentSearchList");
+    var clearButton = document.getElementById("clearStorage");
 
-if(localStorage) {
-  var searchForm = document.getElementById('searchForm');
-  var searchBar = document.getElementById('searchBar');
-  var recentSearchList = document.getElementById('recentSearchList');
-  var clearButton = document.getElementById('clearStorage');
-
-  // Initialize display list
-  var recentSearches = getRecentSearches();
-  recentSearches.forEach(function(searchString) {
-    appendListItem(recentSearchList,searchString);
-  });
-
-  // Set event handlers
-  searchForm.addEventListener('submit', function(event) {
-    var searchString = searchBar.value;
-    if (saveSearchString(searchString)) {
+    // Initialize display list
+    var recentSearches = getRecentSearches();
+    recentSearches.forEach(function (searchString) {
       appendListItem(recentSearchList, searchString);
-    }
-  });
+    });
 
-  clearButton.addEventListener('click', function(event) {
-    removeSearches();
-    clearList(recentSearchList);
-  });
+    // Set event handlers
+    searchForm.addEventListener("submit", function (event) {
+      var searchString = searchBar.value;
+      if (saveSearchString(searchString)) {
+        appendListItem(recentSearchList, searchString);
+      }
+    });
+
+    clearButton.addEventListener("click", function (event) {
+      removeSearches();
+      clearList(recentSearchList);
+    });
   }
 };
