e4606d3da8925273a883c2ed1b3f3bc5e536cc3a
Juvir Monteagudo <john-juvir.M1@trans-cosmos.co.jp>
2020-09-22 10:40:19 +0800

    index page

--- js/app.js
+++ js/app.js
@@ -1,23 +1,29 @@
 $(function(){
     console.log("ready;");
-});
+    let searchCard = $('.content.card');
 
-$(document).ready(function(){
+    searchCard.hide();
     $('#search').keyup(function(){
    
-     // Search text
-     var text = $(this).val().toLowerCase();
-   
-     // Hide all content class element
-     $('.content').hide();
-  
-     // Search 
-     $('.content .title').each(function(){
-   
-      if($(this).text().toLowerCase().indexOf(""+text+"") != -1 ){
-       $(this).closest('.content').show();
-      }
+        // Search text
+        var text = $(this).val().toLowerCase();
       
-    });
-   });
-  });
+        // Hide all content class element
+        $('.content').hide();
+     
+        // Search 
+        $('.content .title').each(function(){
+      
+         if($(this).text().toLowerCase().indexOf(""+text+"") != -1 ){
+          $(this).closest('.content').show();
+         }
+        //  else{
+        //      searchCard.hide();
+        //  }
+         
+       });
+      });
+});
+// $(document).ready(function(){
+   
+//   });
