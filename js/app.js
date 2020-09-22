420cffbc969191dbfefaf85ef7bb5d88a5ef738e
James Dollente <psalmybald@yahoo.com>
2020-09-22 09:01:43 +0800

    function:add func searchbox

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 $(function(){
     console.log("ready;");
 });
+
+$(document).ready(function(){
+    $('#search').keyup(function(){
+   
+     // Search text
+     var text = $(this).val().toLowerCase();
+   
+     // Hide all content class element
+     $('.content').hide();
+  
+     // Search 
+     $('.content .title').each(function(){
+   
+      if($(this).text().toLowerCase().indexOf(""+text+"") != -1 ){
+       $(this).closest('.content').show();
+      }
+    });
+   });
+  });
